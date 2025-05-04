61a4d995b04dcd1af9a942dd9454203c51684f87
Kiersten Marr <45204998+kmarr21@users.noreply.github.com>
2025-05-04 15:48:49 -0700

    Update generate_plots_6k.sh

--- generate_plots_6k.sh
+++ generate_plots_6k.sh
@@ -16,22 +16,12 @@ python new_plot_results.py "${EXPERIMENTS[@]}"
 
 echo "Plots generated in plots/ directory"
 
-# Create a directory to store the plots for downloading
-mkdir -p ~/experiment_results/plots_6k
-
-# Copy the plots to the external directory
-for experiment in "${EXPERIMENTS[@]}"; do
-    for template in basic_med sparse_med zipper_med bottleneck_med bottleneck_hard corridors_med; do
-        # Create destination directory
-        mkdir -p ~/experiment_results/plots_6k/${experiment}/${template}/
-        
-        # Copy all plot files
-        cp -r plots/${experiment}/${template}/* ~/experiment_results/plots_6k/${experiment}/${template}/
-        
-        # Also create zip archives for easier downloading
-        zip -r ~/experiment_results/plots_6k/${experiment}_${template}.zip plots/${experiment}/${template}/
-    done
-done
-
-echo "Plots copied to ~/experiment_results/plots_6k/"
-echo "Zip archives also created in the same directory"
+# If you want to create zip files for easy downloading, uncomment these lines:
+# for experiment in "${EXPERIMENTS[@]}"; do
+#     for template in basic_med sparse_med zipper_med bottleneck_med bottleneck_hard corridors_med; do
+#         # Create zip archives for easier downloading
+#         zip -r plots/${experiment}_${template}.zip plots/${experiment}/${template}/
+#     done
+# done
+# 
+# echo "Zip archives created in plots/ directory"
