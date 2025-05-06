057d456152f6514b295006a48ff69240c7659142
Kiersten Marr <45204998+kmarr21@users.noreply.github.com>
2025-05-06 16:01:31 -0700

    Update generate_neuro_symbolic_plots.sh

--- generate_neuro_symbolic_plots.sh
+++ generate_neuro_symbolic_plots.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # generate_neuro_symbolic_plots.sh
-# Script to generate aggregated plots from neurosymbolic experiment results
+# generates aggregated plots from neurosymbolic experiment results
 
 echo "Generating plots for neurosymbolic experiment..."
 
-# Run the Python script to generate plots
+# run python script to generate plots
 python plot_neuro_symbolic_results.py
 
 echo "Plots generated in plots/neuro_symbolic directory"
